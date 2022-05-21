cdfb5d6ba723ef238104482b0bf1caa5f5a000d2
unknown <sasamilicicbdp@gmail.com>
2022-05-21 14:01:41 +0200

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <header>
+        <h1> Todo List</h1>
+        <Form />
+        <TodoList />
+      </header>
     </div>
   );
 }
